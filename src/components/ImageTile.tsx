4ed80e14b6123ed87fca18ccb9358ef391712a8f
Chris_Cunningham <this.cunningham@gmail.com>
2023-02-20 13:55:20 -0500

    early return for "/by/" route

--- src/components/ImageTile.tsx
+++ src/components/ImageTile.tsx
@@ -3,7 +3,7 @@ import { BlogImage } from 'src/app/interfaces_blog';
 import styles from './ImageTile.module.css';
 
 export const ImageTile = ({ photo }: { photo: BlogImage }) => (
-  <Link href={ `/photos/${photo._id ?? null}` }>
+  <Link href={ `/photos/${photo._id}` }>
     <img
       className={ styles.imageTile }
       src={photo.image.asset.url}

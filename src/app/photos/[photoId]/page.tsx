5589d6ab375a0f24ce2c31afed924b251ad037e5
Chris_Cunningham <this.cunningham@gmail.com>
2023-02-20 14:43:53 -0500

    refactor photoid route

--- src/app/photos/[photoId]/page.tsx
+++ src/app/photos/[photoId]/page.tsx
@@ -2,20 +2,26 @@ import { BlogImage } from 'src/app/interfaces_blog';
 import { client } from 'src/lib/sanity.client';
 
 export default async function DisplayImage ({ params }: { params: { photoId: string }}) {
-  const [{ image, author, caption, imageTags, location }]: BlogImage[] = await client.fetch(`*[_type == "blogImage" && _id == "${params.photoId}"]{
-    author->{name, slug},
-    image{ asset->{ url } },
-    imageTags,
-    caption,
-    location->{ locationName }
-  }`);
+  const blogImages: BlogImage[] = await client.fetch(`
+    *[_type == "blogImage" && _id == "${params.photoId}"]{
+      author->{name, slug},
+      image{ asset->{ url } },
+      imageTags,
+      caption,
+      location->{ locationName }
+    }
+  `);
 
   return (
     <>
-      <img src={ image.asset.url } alt="" style={{ width: 'auto', maxHeight: '70vh' }}/>
-      <p>Photo by: { author.name } { !!location ? ` - Taken in: ${ location?.locationName }`: null}</p>
-      <p>{ caption }</p>
-      <p>Tags: { imageTags }</p>
+      { blogImages.map(({ image, author, location, caption, imageTags }) => (
+        <>
+          <img src={ image.asset.url } alt="" style={{ width: 'auto', maxHeight: '70vh' }}/>
+          <p>Photo by: { author.name } { !!location ? ` - Taken in: ${ location?.locationName }`: null}</p>
+          <p>{ caption }</p>
+          <p>Tags: { imageTags }</p>
+        </>
+      ))}
     </>
   );
 }

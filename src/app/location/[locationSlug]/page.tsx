571a3847ac34c05349233ad0c12fb8c66bcb8d09
Chris_Cunningham <this.cunningham@gmail.com>
2023-02-20 14:55:52 -0500

    refactor locations jsx

--- src/app/location/[locationSlug]/page.tsx
+++ src/app/location/[locationSlug]/page.tsx
@@ -1,6 +1,6 @@
 import { PublishedLocation as PublishedLocation } from 'src/app/interfaces_blog';
-// import { BlogPost } from 'src/components/BlogPost';
-// import { ImageTile } from 'src/components/ImageTile';
+import { BlogPost } from 'src/components/BlogPost';
+import { ImageTile } from 'src/components/ImageTile';
 import { client } from 'src/lib/sanity.client';
 
 export default async function Location ({ params }: { params: { locationSlug: string }}) {
@@ -35,29 +35,33 @@ export default async function Location ({ params }: { params: { locationSlug: st
       }
     }
   }`);
-  console.log(publishedLocations);
-  return (
-    <div>howdy</div>
-  );
-  // return (
-  //   <>
-  //     { publishedLocations[0]?.locationBlogPosts?.length == 0 ? <h3>No blog posts from { locationName }</h3> : (
-  //       <>
-  //         <h3>BlogPosts from { locationName }</h3>
-  //         { locationBlogPosts.map(blogPost => (
-  //           <BlogPost blogPost={ blogPost } key={ blogPost._id }/>
-  //         )) }
-  //       </>
-  //     )}
 
-  //     { locationImages.length == 0 ? <h3>No images from {  locationName }</h3> : (
-  //       <>
-  //         <h3>Images from { locationName }</h3>
-  //         { locationImages.map(image => (
-  //           <ImageTile photo={ image } key={ image._id }/>
-  //         ))}
-  //       </>
-  //     )}
-  //   </>
-  // );
+  return (
+    <>
+      { publishedLocations.map(({ locationName, locationBlogPosts, locationImages, }) => (
+        <>
+          { locationBlogPosts?.length == 0 ?
+            <h3 key={ locationName }>No blog posts from { locationName }</h3>
+            : (
+            <>
+              <h3>BlogPosts from { locationName }</h3>
+              { locationBlogPosts.map(blogPost => (
+                <BlogPost blogPost={ blogPost } key={ blogPost._id }/>
+              )) }
+            </>
+          )}
+
+          { locationImages.length == 0 ? <h3>No images from { locationName }</h3> : (
+            <>
+              <h3>Images from { locationName }</h3>
+              { locationImages.map(image => (
+                <ImageTile photo={ image } key={ image._id }/>
+              ))}
+            </>
+          )}
+
+        </>
+      ))}
+    </>
+  );
 }

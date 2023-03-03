819e4e848762f3b772827bde2b77d431b7f01b11
Chris_Cunningham <this.cunningham@gmail.com>
2023-03-02 21:52:21 -0500

    remove location route and make all location content appear under google map

--- src/app/where-are-we/page.tsx
+++ src/app/where-are-we/page.tsx
@@ -1,21 +1,85 @@
 import { client } from 'src/lib/sanity.client';
 import GoogleMaps from './googleMaps';
 
-import { PublishedLocation as BlogLocation } from '../interfaces_blog';
+import { PublishedLocation } from '../interfaces_blog';
+import { BlogPost } from 'src/components/BlogPost';
+import { ImageTile } from 'src/components/ImageTile';
 
 export default async function WhereAreWe () {
-  const locations: BlogLocation[] = await client.fetch(`*[_type == "location"]{
-    _type,
-    _createdAt,
+  const publishedLocations: PublishedLocation[] = await client.fetch(`
+  *[_type == "location"]{
     locationName,
     mapLocation,
     slug{ current },
+    "locationBlogPosts": *[_type=='blogPost' && references(^._id)]{
+      _id,
+      title,
+      mainImage->{_createdAt, caption, image{ asset->{ path, url } }, author->{ name, slug } },
+      author->,
+      body[]{
+        _type == 'blogImageRef' => @->{
+          caption,
+          image{
+            asset->
+          },
+          imageTags
+        },
+        _type == 'imageCollectionRef' => @->{
+          _id,
+          collectionName,
+          collectionImages[]->{
+            caption,
+            image{
+              asset->
+            },
+            imageTags
+          },
+        },
+        _type != 'reference' => @,
+      },
+      slug,
+      publishedAt,
+      categories,
+  },
+    "locationImages": *[_type=='blogImage' && references(^._id)]{
+      _id,
+      image{
+        asset->
+      }
+    }
   }`);
 
   return (
     <div style={{ width: '100%' }}>
       <h1>Location</h1>
-      <GoogleMaps locations={ locations } />
+      <GoogleMaps locations={ publishedLocations }>
+
+        { publishedLocations.map(({ locationName, locationBlogPosts, locationImages, _id }) => (
+          <div data-location={locationName} key={ _id }>
+            { locationBlogPosts?.length == 0 ?
+              <h3 key={ locationName }>No blog posts from { locationName }</h3>
+              : (
+              <>
+                <h3>BlogPosts from { locationName }</h3>
+                { locationBlogPosts.map(blogPost => (
+                  <BlogPost blogPost={ blogPost } key={ blogPost._id }/>
+                )) }
+              </>
+            )}
+
+            { locationImages.length == 0 ? <h3>No images from { locationName }</h3> : (
+              <>
+                <h3>Images from { locationName }</h3>
+                { locationImages.map(image => (
+                  <ImageTile photo={ image } key={ image._id }/>
+                ))}
+              </>
+            )}
+
+          </div>
+        ))}
+
+      </GoogleMaps>
     </div>
   );
-}
+};

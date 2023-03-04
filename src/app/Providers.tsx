935405e3dd05e633270f05bb9f8bfb64dba12a55
Chris_Cunningham <this.cunningham@gmail.com>
2023-03-04 14:06:09 -0500

    comment out sidebar provider

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -5,22 +5,22 @@ import { createContext, Dispatch, SetStateAction, useState } from 'react';
 type SiteSectionNavSlug = 'safari'| 'blog' |  'photos' | 'where-are-we' | 'about-us';
 
 const NavContext = createContext<[SiteSectionNavSlug, any]>(['blog', null]);
-const SidebarContext = createContext<null | {}>(null);
+// const SidebarContext = createContext<null | {}>(null);
 
 export const NavAndSideBarProviders = ({ children }: { children: React.ReactNode }) => {
   const [selectedNavItem, setSelectedNavItem] = useState<SiteSectionNavSlug>('blog');
-  const [sidebarState, setSideBarState] = useState({
-    tags: null,
-    searchText: '',
-  });
+  // const [sidebarState, setSideBarState] = useState({
+  //   tags: null,
+  //   searchText: '',
+  // });
 
   return (
     <NavContext.Provider value={ [
       selectedNavItem, (setSelectedNavItem) as Dispatch<SetStateAction<SiteSectionNavSlug>>
     ] }>
-      <SidebarContext.Provider value={ sidebarState }>
+      {/* <SidebarContext.Provider value={ sidebarState }> */}
         { children }
-      </SidebarContext.Provider>
+      {/* </SidebarContext.Provider> */}
     </NavContext.Provider>
   );
 };

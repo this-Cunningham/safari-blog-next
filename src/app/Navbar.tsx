d85ea121701ba802a40e703857e943ef9bda0044
Chris_Cunningham <this.cunningham@gmail.com>
2023-03-12 23:27:38 -0400

    add reduce motion query to animation

--- src/app/Navbar.tsx
+++ src/app/Navbar.tsx
@@ -25,7 +25,8 @@ export const Navbar = async () => {
       className='h-12 sm:h-16 fixed left-0 top-0 w-full bg-skyPrimary-900 bg-opacity-95 backdrop-blur-lg text-yellowAccent-400 flex justify-around items-center shadow-[0_4px_8px_rgba(28,35,49,.7)] text-xs sm:text-base'
     >
       { navBar.map((siteSection, index) => (
-        <div className='opacity-0 animate-fadeIn hover:underline'
+        <div
+          className='motion-safe:opacity-0 motion-safe:animate-fadeIn hover:underline motion-reduce:opacity-100'
           key={ siteSection._id }
           style={{ animationDelay: `${(index / 4)}s` }}
         >

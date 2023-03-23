01a6b6fd31bd6bd5c32e3403ffa1abc377823b24
Chris_Cunningham <this.cunningham@gmail.com>
2023-03-22 21:10:59 -0400

    add latest posts

--- src/app/blog/[blogPostSlug]/layout.tsx
+++ src/app/blog/[blogPostSlug]/layout.tsx
@@ -1,15 +1,20 @@
 import Link from 'next/link';
 import { ReactNode } from 'react';
+import { LatestPosts } from 'src/components/BlogPostTile';
 
 export default async function BlogPostLayout ({ children }: { children: ReactNode }) {
 
   return (
-    <div className='relative w-full max-w-7xl my-0 mx-auto bg-skyPrimary-50 p-3 sm:p-8 rounded drop-shadow-md'>
-      <Link href='/blog'
-        className='absolute hidden sm:block sm:top-5 sm:left-5 font-light font-sans text-xs hover:underline'>
-        {'<--'} Back to posts
-      </Link>
-      { children }
-    </div>
+    <>
+      <div className='relative w-full max-w-7xl my-0 mx-auto bg-skyPrimary-50 p-3 sm:p-8 rounded drop-shadow-md'>
+        <Link href='/blog'
+          className='absolute hidden sm:block sm:top-5 sm:left-5 font-light font-sans text-xs hover:underline'>
+          {'<--'} Back to posts
+        </Link>
+        { children }
+      </div>
+      {/* @ts-expect-error Server Component */}
+      <LatestPosts />
+    </>
   );
 };

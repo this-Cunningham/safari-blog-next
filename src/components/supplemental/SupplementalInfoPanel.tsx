c9ff5b874cd3dc5a906609cb1bd97518c9108c2d
Chris_Cunningham <this.cunningham@gmail.com>
2023-03-22 21:59:44 -0400

    adjust info panel

--- src/components/supplemental/SupplementalInfoPanel.tsx
+++ src/components/supplemental/SupplementalInfoPanel.tsx
@@ -1,6 +1,6 @@
 export const SupplementalInfoPanel = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className='font-serif w-full p-4 sm:p-12 bg-white drop-shadow-sm mt-6'>
+    <div className='max-w-7xl font-serif w-full mx-auto p-4 sm:p-12 bg-skyPrimary-200 sm:mt-6 rounded'>
       { children }
     </div>
   );
